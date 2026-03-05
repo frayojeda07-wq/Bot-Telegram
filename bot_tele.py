41ce705498cc348ea5dd447a605137e215a958a1
frayojeda07-wq <fray.ojeda07@gmail.com>
2026-03-05 11:41:07 -0400

    Update bot_tele.py

--- bot_tele.py
+++ bot_tele.py
@@ -1,29 +1,27 @@
+import os
 import telebot
 from flask import Flask, request
-import os
 
-TOKEN = "8243019091:AAEJWzeJcoFmwVRPZdLCACgsSF7fveS0qq4"
+TOKEN = "8641191453:AAHCr4KDbBjL0Ay5OgSpx8P7QqUSL4wTZCs"
 
 bot = telebot.TeleBot(TOKEN)
+
 app = Flask(__name__)
-
-WEBHOOK_PATH = "/webhook"
-WEBHOOK_URL = f"https://govalue.pythonanywhere.com{WEBHOOK_PATH}"
-
-
-@app.route('/')
-def index():
-    return "Bot funcionando"
-
-
-@app.route(WEBHOOK_PATH, methods=['POST'])
-def webhook():
-    json_str = request.get_data().decode('UTF-8')
-    update = telebot.types.Update.de_json(json_str)
-    bot.process_new_updates([update])
-    return '', 200
-
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Bot funcionando correctamente")
+    bot.reply_to(message, "🤖 Bot funcionando en Render!")
+
+@app.route("/", methods=["GET"])
+def home():
+    return "Bot activo"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    json_str = request.get_data().decode("UTF-8")
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return "OK", 200
+
+if __name__ == "__main__":
+    app.run()
