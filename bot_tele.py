e35bb5a94d584cc83f2bf52a6c43750b17155875
frayojeda07-wq <fray.ojeda07@gmail.com>
2026-03-05 13:27:42 -0400

    Update bot_tele.py

--- bot_tele.py
+++ bot_tele.py
@@ -1,33 +1,34 @@
 import os
-import telebot
-from telebot import types
 from flask import Flask, request
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = "8024972363:AAEsXNGfJCvW6J5UuMp2m_7CgMuYM_XWi5s"
-
-bot = telebot.TeleBot(TOKEN)
+TOKEN = '8641191453:AAHCr4KDbBjL0Ay5OgSpx8P7QqUSL4wTZCs'
 
 app = Flask(__name__)
 
-@bot.message_handler(commands=['/start'])
-def start(message):
-    bot.reply_to(message, "🤖 Bot funcionando en Render!")
+application = Application.builder().token(TOKEN).build()
 
-@app.route("/", methods=["GET"])
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("🤖 Bot funcionando en Render!")
+
+
+application.add_handler(CommandHandler("start", start))
+
+
+@app.route("/")
 def home():
     return "Bot activo"
 
+
 @app.route("/webhook", methods=["POST"])
-def webhook():
-    json_str = request.get_data().decode("utf-8")
-    update = types.Update.de_json(json_str)
-    bot.process_new_updates([update])
-    return "OK", 200
+async def webhook():
+    data = request.get_json(force=True)
 
-@bot.message_handler(func=lambda m: True)
-def debug(message):
-    print("Mensaje:", message.text)
-    bot.reply_to(message, "Recibí: " + message.text)
+    update = Update.de_json(data, application.bot)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    await application.initialize()
+    await application.process_update(update)
+
+    return "ok"
