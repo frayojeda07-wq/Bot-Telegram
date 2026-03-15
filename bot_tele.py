974ae3f6a6c31933ec63b539d8fdb80eaadf0b86
frayojeda07-wq <fray.ojeda07@gmail.com>
2026-03-14 22:14:38 -0400

    Fix formatting issues in iniciar_venta function

--- bot_tele.py
+++ bot_tele.py
@@ -291,8 +291,8 @@ async def iniciar_venta(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     conn = sqlite3.connect('ventas.db')
     cursor = conn.cursor()
-    user_tienda = update.effective_user.id
-    cursor.execute("SELECT producto, precio FROM precios WHERE user_telegran_id = ?", (user_tienda,))")
+    user_tienda = update.effective_user.id 
+    cursor.execute("SELECT producto, precio FROM precios WHERE user_telegran_id = ?", (user_tienda,))
     productos = cursor.fetchall()
     conn.close()
 
